20ca38adac0da0284d5a6a0ab77c8509228bb132
Aubry Franquinet <aubry.franquinet@hotmail.com>
2021-12-02 10:56:04 +0100

    added some functions

--- index..mjs
+++ index..mjs
@@ -14,5 +14,30 @@ async function getAllUsers() {
     // When the transaction is over, we close the connection to the DB
     db.close();
 
-    return allEmployees;
+    return allUsers;
 }
+
+async function getUserByName(name) {
+    const db = await SQLite.open("./db/database");
+    const Users= await db.get("SELECT * FROM Users WHERE first_name=?", [
+        name,
+    ]);
+    console.log(Users);
+
+    db.close();
+    return Users;
+}
+
+async function insertEvent(name) {
+    const db = await SQLite.open("./db/database");
+    const inputEvent = await db.get("INSERT INTO dates_by_event VALUES(id, event_id, event_date);");
+    console.log(inputEvent);
+
+    db.close();
+    return Users;
+}
+
+
+
+
+
